5fdd262670591f99895ea9baf94cf80c0e6dbbb0
ihleung <ihfungleung@gmail.com>
2024-05-07 10:56:21 -0400

    updated

--- spinner1.js
+++ spinner1.js
@@ -1,4 +1,17 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
+/*
+How this works:
+
+Each character is a delayed write, using setTimeout
+
+With each delayed write, we start with returning the cursor to the beginning
+
+We then write the next character in the animation
+
+Spaces are added after the character so that the cursor moves far enough away from our animation
+
+Removing the space padding at the end of the strings will mean that the cursor will sit on top of the animation, making it harder to see
+*/
 
 setTimeout(() => { //start of the spinning line animation
   process.stdout.write('\r|   ');
@@ -36,3 +49,19 @@ setTimeout(() => {
 setTimeout(() => {
   process.stdout.write('\r|   ');
 },  1700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+} , 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+} , 2500);
